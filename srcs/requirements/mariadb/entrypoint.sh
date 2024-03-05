ad7e6817664d54cbf42d61ca51d4d5efb63d8b61
Théo ZANCHI <tzanchi@student.42berlin.de>
2024-03-05 17:08:13 +0100

    first config try of mariadb

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,1 +1,17 @@
 #! /usr/bin/bash
+
+service mariadb start
+sleep 2
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+
+mariadb -e "CREATE USER IF NOT EXISTS \`${USER_NAME}\`@'localhost' IDENTIFIED BY '${MARIADB_PWD}';"
+
+mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${USER_NAME}\`@'%' IDENTIFIED BY '${MARIADB_PWD}';"
+
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}';"
+
+mariadb -e "FLUSH PRIVILEGES;"
+
+mysqladmin -u root -p${MARIADB_ROOT_PWD} shutdown
+
+exec mysqld_safe
