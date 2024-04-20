0f276c2e7d483312df8c7ded774db87cfae2ff74
Théo ZANCHI <tzanchi@student.42berlin.de>
2024-04-20 18:11:48 +0200

    try with new conf file

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -2,18 +2,24 @@
 
 service mariadb start
 
-# sleep infinity
+while ! mysqladmin ping -h localhost --silent; do
+	sleep 1
+done
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mariadb -u root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
 
-mariadb -e "CREATE USER IF NOT EXISTS \`${USER_NAME}\`@'localhost' IDENTIFIED BY '${MARIADB_PWD}';"
+mariadb -u root -e "CREATE USER IF NOT EXISTS \`${USER_NAME}\`@'localhost' IDENTIFIED BY '$(cat ${MARIADB_PWD})';"
 
-mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${USER_NAME}\`@'%' IDENTIFIED BY '${MARIADB_PWD}';"
+mariadb -u root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${USER_NAME}\`@'%' IDENTIFIED BY '$(cat ${MARIADB_PWD})';"
 
-mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PWD}';"
+mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat ${MARIADB_ROOT_PWD})';"
 
-mariadb -e "FLUSH PRIVILEGES;"
+mariadb -u root -p$(cat ${MARIADB_ROOT_PWD}) -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p${MARIADB_ROOT_PWD} shutdown
+echo "Finished configuration, now restarting the server..."
+
+mysqladmin -u root -p$(cat ${MARIADB_ROOT_PWD}) shutdown
+
+echo "Server restarted, now running in the background..."
 
 exec mysqld_safe
