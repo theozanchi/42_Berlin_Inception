9a9c387bde52b85a844ebdab11f68d4540a8176a
theozanchi <theo.zanchi@gmail.com>
2024-04-21 17:42:50 +0200

    MariaDB is working on macOS, need to try again on Linux before doing the pull request

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,25 +1,21 @@
-#! /usr/bin/bash
+#!/bin/bash
 
 service mariadb start
 
+# Waiting for MariaDB to be ready before appying any modification
 while ! mysqladmin ping -h localhost --silent; do
 	sleep 1
 done
 
-mariadb -u root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+# Configuring MariaDB
+mariadb -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
+mariadb -e "CREATE USER IF NOT EXISTS \`${USER_NAME}\`@'localhost' IDENTIFIED BY '$(cat ${MARIADB_PWD})';"
+mariadb -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${USER_NAME}\`@'%' IDENTIFIED BY '$(cat ${MARIADB_PWD})';"
+mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat ${MARIADB_ROOT_PWD})';"
 
-mariadb -u root -e "CREATE USER IF NOT EXISTS \`${USER_NAME}\`@'localhost' IDENTIFIED BY '$(cat ${MARIADB_PWD})';"
-
-mariadb -u root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${USER_NAME}\`@'%' IDENTIFIED BY '$(cat ${MARIADB_PWD})';"
-
-mariadb -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat ${MARIADB_ROOT_PWD})';"
-
+# Forcing changes to be immediately applied
 mariadb -u root -p$(cat ${MARIADB_ROOT_PWD}) -e "FLUSH PRIVILEGES;"
 
-echo "Finished configuration, now restarting the server..."
-
+# Restarting MariaDB
 mysqladmin -u root -p$(cat ${MARIADB_ROOT_PWD}) shutdown
-
-echo "Server restarted, now running in the background..."
-
 exec mysqld_safe
