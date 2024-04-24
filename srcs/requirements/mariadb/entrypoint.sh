9c764cf0fc7053aca253983012aa628fe6b68bfe
Théo ZANCHI <tzanchi@student.42berlin.de>
2024-04-24 09:43:30 +0200

    fixed mariadb bug, db now created properly

--- srcs/requirements/mariadb/entrypoint.sh
+++ srcs/requirements/mariadb/entrypoint.sh
@@ -1,19 +1,11 @@
 #!/bin/sh
 
-if [ ! -d /var/lib/mysql/$DB_NAME ]; then
-	service mariadb start
+# Create the SQL init script
+echo "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;" > /etc/mysql/init.sql
+echo "CREATE USER IF NOT EXISTS '${DB_USER}'@'%' IDENTIFIED BY '$(cat $DB_PWD)';" >> /etc/mysql/init.sql
+echo "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO '${DB_USER}'@'%' IDENTIFIED BY '$(cat $DB_PWD)';" >> /etc/mysql/init.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat $DB_ROOT_PWD)';" >> /etc/mysql/init.sql
+echo "FLUSH PRIVILEGES;" >> /etc/mysql/init.sql
 
-	while ! mysqladmin ping -h localhost --silent; do
-		sleep 1
-	done
-
-	mariadb -u root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`; \
-						CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '$(cat $DB_PWD)'; \
-						GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${USER_NAME}\`@'%' IDENTIFIED BY '$(cat $DB_PWD)'; \
-						ALTER USER 'root'@'localhost' IDENTIFIED BY '$(cat $DB_ROOT_PWD)'; \
-						FLUSH PRIVILEGES;"
-
-	mysqladmin --user=root --password=$(cat ${DB_ROOT_PWD}) shutdown
-fi
-
+# Start the MariaDB server
 exec mysqld_safe
--- srcs/requirements/nginx/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/bash
-
-set -e
-
-nginx -g "daemon off;"
